628a8499e7986d9db30361817156f41686532d3b
Axell505 <axelcrbl505@hotmail.com>
2024-08-29 14:54:51 -0300

    Hice models.py

--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Mensaje(models.Model):
+    texto = models.TextField()
+    remitente = models.CharField(max_length=100)
+    destinatario = models.CharField(max_length=100)
+    fecha_y_hora = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return f"{self.texto[:20]} - {self.remitente} a {self.destinatario}"
