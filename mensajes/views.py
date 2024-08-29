a3919f204deed132fd2033ef5f7b91dafbac7169
Axell505 <axelcrbl505@hotmail.com>
2024-08-29 17:22:20 -0300

    Hice la vista y cree urls.py en mensajes, además de cambiar urls.py en TableroMensajes

--- mensajes/views.py
+++ mensajes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Mensaje
 
-# Create your views here.
+def mensajes_recibidos(request):
+    destinatario = request.GET.get('destinatario')  # Captura el destinatario desde los parámetros de la URL
+    mensajes = Mensaje.objects.filter(destinatario=destinatario)  # Filtra los mensajes por destinatario
+    return render(request, 'mensajes_recibidos.html', {'mensajes': mensajes})
+
+def home(request):
+    return HttpResponse("¡Bienvenido a la página principal!")
