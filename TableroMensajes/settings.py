c2de85ca4d550756f1e4eaed79603aa2f06bcba5
Axell505 <axelcrbl505@hotmail.com>
2024-08-29 17:41:30 -0300

    Cambié los ajsutes en Admin.py para poder usar el administrador de Django para crear mensajes, al igual que cree un superusario que usaré para acceder de nombre Axel con la contraseña django123.

--- TableroMensajes/settings.py
+++ TableroMensajes/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TableroMensajes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['mensajes\templates'],
+        'DIRS': ['mensajes\\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mensajes/admin.py
+++ mensajes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Mensaje
 
-# Register your models here.
+@admin.register(Mensaje)
+class MensajeAdmin(admin.ModelAdmin):
+    list_display = ('texto', 'remitente', 'destinatario', 'fecha_y_hora')
+    search_fields = ('texto', 'remitente', 'destinatario')
